c14f12d82cf1ef573f77431f7a054482f98795f8
David <darknight4123@gmail.com>
2021-10-01 01:56:12 +0300

    Removed the job type from the completion message.

--- src/main/java/dte/employme/messages/Message.java
+++ src/main/java/dte/employme/messages/Message.java
@@ -14,7 +14,7 @@ public enum Message
 {
 	//Jobs
 	JOB_ADDED_TO_BOARD("&aYour &e%s &aOffer was added to the &eJobs Board&a."),
-	JOB_SUCCESSFULLY_COMPLETED(GREEN + "You successfully completed a %s Job!"),
+	JOB_SUCCESSFULLY_COMPLETED(GREEN + "You successfully completed a Job!"),
 	JOB_SUCCESSFULLY_DELETED(YELLOW + "You successfully deleted this Job!"),
 
 	//Rewards
